56666c2e402d3cc7af59195cd7f5dd378dfbd768
Your Name <you@example.com>
2018-07-18 10:02:27 +0800

    07-18

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import DocumentTitle from 'react-document-title';
 import LoginScreen from './Screen/LoginScreen';
 import RegisterScreen from './Screen/RegisterScreen';
 import CreateUserScreen from './Screen/CreateUserScreen';
-import TabBarScreen from './Screen/TabBarScreen';
+//import TabBarScreen from './Screen/TabBarScreen';
+import CreateMessageScreen from './Screen/CreateMessageScreen';
 
 class App extends Component {
   render() {
@@ -16,7 +17,7 @@ class App extends Component {
           <Route path={'/LoginScreen'} component={LoginScreen} />
           <Route path={'/RegisterScreen'} component={RegisterScreen} />
           <Route path={'/CreateUserScreen'} component={CreateUserScreen} />
-          <Route path={'/TabBarScreen'} component={TabBarScreen} />
+          <Route path={'/CreateMessageScreen'} component={CreateMessageScreen} />
         </div>
     );
   }
